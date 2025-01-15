c9973341be06b1d59622fb5b24554945c2d4e97c
guiruppel05 <guirapha_ruppel.com>
2025-01-14 22:55:51 -0300

    finalfinal

--- scriptfeedback.js
+++ scriptfeedback.js
@@ -1,38 +1,26 @@
-document.getElementById("feedbackForm").addEventListener("submit", function (event) {
-    event.preventDefault();
+document.getElementById('feedbackForm').addEventListener('submit', function(e) {
+       e.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const phone = document.getElementById("phone").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
+       const name = document.getElementById('name').value;
+       const message = document.getElementById('message').value;
 
-    const data = {
-        name,
-        phone,
-        email,
-        message,
-    };
-
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbwk_mjq8R-9o9ui4f26z9h6YeZ1Bv3tChZTLduJapWkFt5bPGdCwiX2PJbSuOOEjfzH/exec'; // Substitua pela URL do seu script
-    const responseMessage = document.getElementById("responseMessage");
-
-    fetch(scriptURL, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then((response) => {
-            if (response.ok) {
-                responseMessage.textContent = "Feedback enviado com sucesso!";
-                document.getElementById("feedbackForm").reset();
-            } else {
-                responseMessage.textContent = "Erro ao enviar o feedback. Tente novamente.";
-            }
-        })
-        .catch((error) => {
-            responseMessage.textContent = "Erro ao enviar o feedback. Tente novamente.";
-            console.error("Erro:", error);
-        });
-});
+       fetch('https://script.google.com/macros/s/AKfycbz0HPhQ5bRUtKJrqS_WRAGTCU-_DmmviscxaCkvcqVXZsQRsKDD9W7u96OQKMvfxsYi/exec', {
+           method: 'POST',
+           headers: {
+               'Content-Type': 'application/json',
+           },
+           body: JSON.stringify({
+               name: name,
+               message: message,
+           }),
+       })
+       .then(response => response.json())
+       .then(data => {
+           console.log('Sucesso:', data);
+           alert('Feedback enviado com sucesso!');
+       })
+       .catch((error) => {
+           console.error('Erro:', error);
+           alert('Falha ao enviar feedback.');
+       });
+    });
