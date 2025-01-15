2ad3594fb0208e2dda401f77d53d9d6e2649b4cf
guiruppel05 <guirapha_ruppel.com>
2025-01-14 23:03:42 -0300

    ...

--- scriptfeedback.js
+++ scriptfeedback.js
@@ -1,26 +1,35 @@
-document.getElementById('feedbackForm').addEventListener('submit', function(e) {
-       e.preventDefault();
+document.getElementById('feedbackForm').addEventListener('submit', function (e) {
+    e.preventDefault();
 
-       const name = document.getElementById('name').value;
-       const message = document.getElementById('message').value;
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
 
-       fetch('https://script.google.com/macros/s/AKfycbz0HPhQ5bRUtKJrqS_WRAGTCU-_DmmviscxaCkvcqVXZsQRsKDD9W7u96OQKMvfxsYi/exec', {
-           method: 'POST',
-           headers: {
-               'Content-Type': 'application/json',
-           },
-           body: JSON.stringify({
-               name: name,
-               message: message,
-           }),
-       })
-       .then(response => response.json())
-       .then(data => {
-           console.log('Sucesso:', data);
-           alert('Feedback enviado com sucesso!');
-       })
-       .catch((error) => {
-           console.error('Erro:', error);
-           alert('Falha ao enviar feedback.');
-       });
-    });
+    const scriptURL =
+        'https://script.google.com/macros/s/AKfycbz0HPhQ5bRUtKJrqS_WRAGTCU-_DmmviscxaCkvcqVXZsQRsKDD9W7u96OQKMvfxsYi/exec';
+    const responseMessage = document.getElementById('responseMessage');
+
+    fetch(scriptURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, phone, email, message }),
+    })
+        .then((response) => {
+            if (response.ok) {
+                responseMessage.textContent = 'Feedback enviado com sucesso!';
+                responseMessage.style.color = 'green';
+                document.getElementById('feedbackForm').reset();
+            } else {
+                responseMessage.textContent = 'Erro ao enviar o feedback.';
+                responseMessage.style.color = 'red';
+            }
+        })
+        .catch((error) => {
+            console.error('Erro:', error);
+            responseMessage.textContent = 'Erro ao enviar o feedback.';
+            responseMessage.style.color = 'red';
+        });
+});
