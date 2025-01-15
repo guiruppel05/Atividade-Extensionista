f59dc6de352baa5142197fb5b933e6ff8b80521d
guiruppel05 <guirapha_ruppel.com>
2025-01-14 21:21:49 -0300

    forms

---
+++ scriptfeedback.js
@@ -0,0 +1,38 @@
+document.getElementById("feedbackForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    const data = {
+        name,
+        phone,
+        email,
+        message,
+    };
+
+    const scriptURL = 'AKfycbw34iFzG4VEaUt_VjQylMeBtXlGNxOqMm8eDEhL4bckLRT27Ehj0h9uYdmrc0WdUWx3'; // Substitua pela URL do seu script
+    const responseMessage = document.getElementById("responseMessage");
+
+    fetch(scriptURL, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((response) => {
+            if (response.ok) {
+                responseMessage.textContent = "Feedback enviado com sucesso!";
+                document.getElementById("feedbackForm").reset();
+            } else {
+                responseMessage.textContent = "Erro ao enviar o feedback. Tente novamente.";
+            }
+        })
+        .catch((error) => {
+            responseMessage.textContent = "Erro ao enviar o feedback. Tente novamente.";
+            console.error("Erro:", error);
+        });
+});
