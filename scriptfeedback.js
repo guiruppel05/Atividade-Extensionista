9da0c93e3f4bcd6058aefb0dd81ccf3676b3000c
guiruppel05 <guirapha_ruppel.com>
2025-01-15 23:33:31 -0300

    python

--- scriptfeedback.js
+++ scriptfeedback.js
@@ -6,37 +6,29 @@ document.getElementById('feedbackForm').addEventListener('submit', function (e) 
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    const airtableURL = 'https://api.airtable.com/v0/appZGkNEJHaGPBMpG/Feedbacks%20Form'; // Substitua pelo ID da base e tabela
-    const personalAccessToken = 'patXykrQOYqvn3A0x.c99051ed93d37d2a9d5a0b4f51255068f9cc6051b08b4d2ef6423de101642a76'; // Substitua pelo token gerado
+    const scriptURL = 'https://guiruppel05.pythonanywhere.com/submit_feedback';
+    const responseMessage = document.getElementById('responseMessage');
 
-    const data = {
-        fields: {
-            Name: name,
-            Phone: phone,
-            Email: email,
-            Message: message,
-            Date: new Date().toISOString(),
-        },
-    };
-
-    fetch(airtableURL, {
+    fetch(scriptURL, {
         method: 'POST',
         headers: {
-            Authorization: `Bearer ${personalAccessToken}`,
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify({ name, phone, email, message }),
     })
         .then((response) => {
             if (response.ok) {
-                alert('Feedback enviado com sucesso!');
+                responseMessage.textContent = 'Feedback enviado com sucesso!';
+                responseMessage.style.color = 'green';
                 document.getElementById('feedbackForm').reset();
             } else {
-                alert('Erro ao enviar o feedback.');
+                responseMessage.textContent = 'Erro ao enviar o feedback.';
+                responseMessage.style.color = 'red';
             }
         })
         .catch((error) => {
             console.error('Erro:', error);
-            alert('Erro ao enviar o feedback.');
+            responseMessage.textContent = 'Erro ao enviar o feedback.';
+            responseMessage.style.color = 'red';
         });
 });
